8aaebe5465aabdbce76f3c4dace20ff568d3a9ca
Alexandre Benoit <t-albeno@microsoft.com>
2018-07-25 19:31:49 -0700

    Just dropping that code for later use

---
+++ SkimThrough/KeyProcessorImplementation.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SkimThrough
+{
+    public class KeyProcessorImplementation : KeyProcessor
+    {
+        private IWpfTextView wpfTextView;
+
+        public KeyProcessorImplementation(IWpfTextView wpfTextView)
+        {
+            this.wpfTextView = wpfTextView;
+        }
+
+        public override void KeyDown(KeyEventArgs args)
+        {
+            Debug.WriteLine($"key down {args.Key}, {args.SystemKey}, {args.ImeProcessedKey}");
+        }
+
+        public override bool IsInterestedInHandledEvents => true;
+
+        public override void PreviewKeyDown(KeyEventArgs args)
+        {
+            if (args.Key == Key.System)
+                return;
+            Debug.WriteLine($"preview key down {args.Key}, {args.SystemKey}, {args.ImeProcessedKey}");
+            args.Handled = true;
+        }
+    }
+
+    [Export(typeof(IKeyProcessorProvider))]
+    [Name("The processor")]
+    [TextViewRole(PredefinedTextViewRoles.Interactive)]
+    [ContentType("text")]
+    [Order(Before = "default")]
+    public class KeyProcessorImplementationProvider : IKeyProcessorProvider
+    {
+        public KeyProcessor GetAssociatedProcessor(IWpfTextView wpfTextView)
+        {
+            return new KeyProcessorImplementation(wpfTextView);
+        }
+    }
+}
---
+++ SkimThrough/TextAdornment2.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Formatting;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Utilities;
+
+namespace SkimThrough
+{
+    /// <summary>
+    /// TextAdornment2 places red boxes behind all the "a"s in the editor window
+    /// </summary>
+    internal sealed class TextAdornment2
+    {
+        /// <summary>
+        /// The layer of the adornment.
+        /// </summary>
+        private readonly IAdornmentLayer layer;
+
+        /// <summary>
+        /// Text view where the adornment is created.
+        /// </summary>
+        private readonly IWpfTextView view;
+
+        /// <summary>
+        /// Adornment brush.
+        /// </summary>
+        private readonly Brush brush;
+
+        /// <summary>
+        /// Adornment pen.
+        /// </summary>
+        private readonly Pen pen;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TextAdornment2"/> class.
+        /// </summary>
+        /// <param name="view">Text view to create the adornment for</param>
+        public TextAdornment2(IWpfTextView view)
+        {
+            if (view == null)
+            {
+                throw new ArgumentNullException("view");
+            }
+
+            this.layer = view.GetAdornmentLayer("TextAdornment2");
+            
+this.view = view;
+            this.view.LayoutChanged += this.OnLayoutChanged;
+            this.view.Caret.PositionChanged += this.OnPositionChanged;
+
+            // Create the pen and brush to color the box behind the a's
+            this.brush = new SolidColorBrush(Color.FromArgb(0x20, 0x00, 0x00, 0xff));
+            this.brush.Freeze();
+
+            var penBrush = new SolidColorBrush(Colors.Red);
+            penBrush.Freeze();
+            this.pen = new Pen(penBrush, 0.5);
+            this.pen.Freeze();
+
+            
+        }
+
+        private static bool changing = false;
+        private void OnPositionChanged(object sender, CaretPositionChangedEventArgs e)
+        {
+            if (changing)
+                return;
+            if (e.NewPosition.Equals(e.OldPosition))
+                return;
+            if (e.NewPosition.BufferPosition.GetContainingLine().LineNumber == e.OldPosition.BufferPosition.GetContainingLine().LineNumber)
+                return;
+
+            var diff = e.OldPosition.BufferPosition.Difference(e.NewPosition.BufferPosition);
+            if (diff > 0)
+            {
+                Debug.WriteLine("Down");
+            }
+            else
+            {
+                Debug.WriteLine("Up");
+            }
+
+            changing = false;
+        }
+
+        //
+        // Summary:
+        //     Handles the KeyDown event.
+        //
+        // Parameters:
+        //   args:
+        //     A System.Windows.Input.KeyEventArgs describing the key event.
+        public void KeyDownCustomEvent(KeyEventArgs args)
+        {
+            Console.WriteLine("key down!");
+        }
+
+        /// <summary>
+        /// Handles whenever the text displayed in the view changes by adding the adornment to any reformatted lines
+        /// </summary>
+        /// <remarks><para>This event is raised whenever the rendered text displayed in the <see cref="ITextView"/> changes.</para>
+        /// <para>It is raised whenever the view does a layout (which happens when DisplayTextLineContainingBufferPosition is called or in response to text or classification changes).</para>
+        /// <para>It is also raised whenever the view scrolls horizontally or when its size changes.</para>
+        /// </remarks>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        internal void OnLayoutChanged(object sender, TextViewLayoutChangedEventArgs e)
+        {
+            foreach (ITextViewLine line in e.NewOrReformattedLines)
+            {
+                this.CreateVisuals(line);
+            }
+        }
+
+        /// <summary>
+        /// Adds the scarlet box behind the 'a' characters within the given line
+        /// </summary>
+        /// <param name="line">Line to add the adornments</param>
+        private void CreateVisuals(ITextViewLine line)
+        {
+            IWpfTextViewLineCollection textViewLines = this.view.TextViewLines;
+
+            // Loop through each character, and place a box around any 'a'
+            for (int charIndex = line.Start; charIndex < line.End; charIndex++)
+            {
+                if (this.view.TextSnapshot[charIndex] == 'a')
+                {
+                    SnapshotSpan span = new SnapshotSpan(this.view.TextSnapshot, Span.FromBounds(charIndex, charIndex + 1));
+                    Geometry geometry = textViewLines.GetMarkerGeometry(span);
+                    if (geometry != null)
+                    {
+                        var drawing = new GeometryDrawing(this.brush, this.pen, geometry);
+                        drawing.Freeze();
+
+                        var drawingImage = new DrawingImage(drawing);
+                        drawingImage.Freeze();
+
+                        var image = new Image
+                        {
+                            Source = drawingImage,
+                        };
+
+                        // Align the image with the top of the bounds of the text geometry
+                        Canvas.SetLeft(image, geometry.Bounds.Left);
+                        Canvas.SetTop(image, geometry.Bounds.Top);
+
+                        this.layer.AddAdornment(AdornmentPositioningBehavior.TextRelative, span, null, image, null);
+                    }
+                }
+            }
+        }
+    }
+}
